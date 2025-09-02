97628b28230128ef3296eb99de7e8b1e0864f516
Tom Anderson <tom@ando.sh>
2025-09-02 17:09:27 +1000

    add llvm ty cache

--- src/llvm/mod.rs
+++ src/llvm/mod.rs
@@ -1,12 +1,17 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap};
 
 use inkwell::{
-    AddressSpace, context::Context as IContext, execution_engine::JitFunction,
-    module::Module as IModule, values::BasicValueEnum,
+    builder::Builder,
+    context::Context as IContext,
+    execution_engine::JitFunction,
+    module::Module as IModule,
+    types::{BasicType, BasicTypeEnum},
+    values::{BasicValueEnum, PointerValue},
 };
 
 use crate::ir::{
-    BasicBlock, BinOp, Local, Operand, Projection, RValue, Statement, Terminator, TyInfo, Value,
+    BasicBlock, BinOp, Body, Local, Operand, Place, Projection, RValue, Statement, Terminator, Ty,
+    TyInfo, Value,
     ctx::{Function, IrCtx},
 };
 
@@ -30,12 +35,14 @@ impl<'ir> Llvm<'ir> {
 pub struct Module<'ir, 'ctx> {
     llvm: &'ctx Llvm<'ir>,
     module: IModule<'ctx>,
+    tys: LlvmTys<'ir, 'ctx>,
 }
 impl<'ir, 'ctx> Module<'ir, 'ctx> {
     pub fn new(llvm: &'ctx Llvm<'ir>, name: &str) -> Self {
         Self {
             llvm,
             module: llvm.ctx.create_module(name),
+            tys: LlvmTys::new(llvm),
         }
     }
 
@@ -45,13 +52,11 @@ impl<'ir, 'ctx> Module<'ir, 'ctx> {
         // Capture function body.
         let body = &self.llvm.ir.functions[function];
 
-        let i8_type = self.llvm.ctx.i8_type();
-        let ptr_type = self.llvm.ctx.ptr_type(AddressSpace::default());
-
         // Set up function signature.
+        let ret_ty = self.tys.get(body.local_decls[Local::zero()].ty);
         let function = self
             .module
-            .add_function(name, i8_type.fn_type(&[], false), None);
+            .add_function(name, ret_ty.fn_type(&[], false), None);
 
         // Entry basic block sets up allocations.
         let entry_bb = self.llvm.ctx.append_basic_block(function, "entry");
@@ -60,15 +65,7 @@ impl<'ir, 'ctx> Module<'ir, 'ctx> {
             .local_decls
             .iter_keys()
             .map(|(local, alloc)| {
-                let alloc = builder
-                    .build_alloca(
-                        match &self.llvm.ir.tys.get(alloc.ty) {
-                            TyInfo::U8 | TyInfo::I8 => i8_type,
-                            _ => unimplemented!(),
-                        },
-                        name,
-                    )
-                    .unwrap();
+                let alloc = builder.build_alloca(self.tys.get(alloc.ty), name).unwrap();
 
                 (local, alloc)
             })
@@ -98,76 +95,23 @@ impl<'ir, 'ctx> Module<'ir, 'ctx> {
             for statement in &block.statements {
                 match statement {
                     Statement::Assign { place, rvalue } => {
-                        let place = {
-                            let mut ptr = locals[&place.local];
-
-                            for proj in &place.projection {
-                                match proj {
-                                    Projection::Deref => todo!(),
-                                    Projection::Field(_) => todo!(),
-                                    Projection::Index(local) => todo!(),
-                                }
-                            }
-
-                            ptr
-                        };
+                        let (place, ty) =
+                            self.get_place_ptr(place.clone(), &builder, body, &locals);
 
                         let rvalue = match rvalue {
-                            RValue::Use(operand) => match operand {
-                                Operand::Place(place) => {
-                                    let ptr = locals[&place.local];
-                                    assert!(place.projection.is_empty());
-                                    builder.build_load(i8_type, ptr, "asdfasdf").unwrap()
-                                }
-                                Operand::Constant(value) => match value {
-                                    Value::U8(value) => {
-                                        i8_type.const_int(*value as u64, false).into()
-                                    }
-                                    Value::I8(value) => {
-                                        i8_type.const_int(*value as u64, false).into()
-                                    }
-                                    Value::Ref(pointer) => todo!(),
-                                    Value::Array(values) => todo!(),
-                                },
-                            },
+                            RValue::Use(operand) => {
+                                self.get_operand(operand, &builder, body, &locals)
+                            }
                             RValue::Ref(place) => todo!(),
                             RValue::BinaryOp { op, lhs, rhs } => {
-                                let BasicValueEnum::IntValue(lhs) = (match lhs {
-                                    Operand::Place(place) => {
-                                        let ptr = locals[&place.local];
-                                        assert!(place.projection.is_empty());
-                                        builder.build_load(i8_type, ptr, "asdfasdf").unwrap()
-                                    }
-                                    Operand::Constant(value) => match value {
-                                        Value::U8(value) => {
-                                            i8_type.const_int(*value as u64, false).into()
-                                        }
-                                        Value::I8(value) => {
-                                            i8_type.const_int(*value as u64, false).into()
-                                        }
-                                        Value::Ref(pointer) => todo!(),
-                                        Value::Array(values) => todo!(),
-                                    },
-                                }) else {
+                                let BasicValueEnum::IntValue(lhs) =
+                                    self.get_operand(lhs, &builder, body, &locals)
+                                else {
                                     panic!("not good");
                                 };
-                                let BasicValueEnum::IntValue(rhs) = (match rhs {
-                                    Operand::Place(place) => {
-                                        let ptr = locals[&place.local];
-                                        assert!(place.projection.is_empty());
-                                        builder.build_load(i8_type, ptr, "asdfasdf").unwrap()
-                                    }
-                                    Operand::Constant(value) => match value {
-                                        Value::U8(value) => {
-                                            i8_type.const_int(*value as u64, false).into()
-                                        }
-                                        Value::I8(value) => {
-                                            i8_type.const_int(*value as u64, false).into()
-                                        }
-                                        Value::Ref(pointer) => todo!(),
-                                        Value::Array(values) => todo!(),
-                                    },
-                                }) else {
+                                let BasicValueEnum::IntValue(rhs) =
+                                    self.get_operand(rhs, &builder, body, &locals)
+                                else {
                                     panic!("not good");
                                 };
 
@@ -200,8 +144,14 @@ impl<'ir, 'ctx> Module<'ir, 'ctx> {
                 } => todo!(),
                 Terminator::Goto(basic_block) => todo!(),
                 Terminator::Return => {
+                    let ret_local = Local::zero();
+
                     let out = builder
-                        .build_load(i8_type, locals[&Local::zero()], "idk")
+                        .build_load(
+                            self.tys.get(body.local_decls[ret_local].ty),
+                            locals[&ret_local],
+                            "idk",
+                        )
                         .unwrap();
                     builder.build_return(Some(&out)).unwrap();
                 }
@@ -223,5 +173,132 @@ impl<'ir, 'ctx> Module<'ir, 'ctx> {
             unsafe { engine.get_function(name).unwrap() };
 
         unsafe { f.call() }
+    }
+
+    fn get_place_ptr(
+        &self,
+        place: Place,
+        builder: &Builder<'ctx>,
+        body: &Body,
+        locals: &HashMap<Local, PointerValue<'ctx>>,
+    ) -> (PointerValue<'ctx>, TyInfo) {
+        let mut ptr = locals[&place.local];
+        let mut ty = self.llvm.ir.tys.get(body.local_decls[place.local].ty);
+
+        for projection in place.projection {
+            match projection {
+                Projection::Deref => {
+                    let TyInfo::Ref(inner_ty) = ty else {
+                        panic!("can only deref a reference");
+                    };
+
+                    ptr = builder
+                        .build_load(self.tys.get(inner_ty), ptr, "deref")
+                        .unwrap()
+                        .into_pointer_value();
+                    ty = self.llvm.ir.tys.get(inner_ty);
+                }
+                Projection::Field(_) => todo!(),
+                Projection::Index(local) => {
+                    let array_ty = self.llvm.ir.tys.find_or_insert(ty.clone());
+                    let TyInfo::Array {
+                        ty: inner_ty,
+                        length: _,
+                    } = ty
+                    else {
+                        panic!("can only index an array");
+                    };
+
+                    let index = builder
+                        .build_load(
+                            self.tys.get(body.local_decls[place.local].ty),
+                            locals[&local],
+                            "load index",
+                        )
+                        .unwrap()
+                        .into_int_value();
+
+                    ptr = unsafe {
+                        builder
+                            .build_gep(self.tys.get(array_ty), ptr, &[index], "index somethign")
+                            .unwrap()
+                    };
+                    ty = self.llvm.ir.tys.get(inner_ty);
+                }
+            }
+        }
+
+        (ptr, ty)
+    }
+
+    fn get_operand(
+        &self,
+        operand: &Operand,
+        builder: &Builder<'ctx>,
+        body: &Body,
+        locals: &HashMap<Local, PointerValue<'ctx>>,
+    ) -> BasicValueEnum<'ctx> {
+        match operand {
+            Operand::Place(place) => {
+                let (ptr, ty) = self.get_place_ptr(place.clone(), &builder, body, &locals);
+                builder
+                    .build_load(
+                        self.tys.get(self.llvm.ir.tys.find_or_insert(ty)),
+                        ptr,
+                        "asdfasdf",
+                    )
+                    .unwrap()
+            }
+            Operand::Constant(value) => match value {
+                Value::U8(value) => self
+                    .tys
+                    .get(self.llvm.ir.tys.find_or_insert(TyInfo::U8))
+                    .into_int_type()
+                    .const_int(*value as u64, false)
+                    .into(),
+                Value::I8(value) => self
+                    .tys
+                    .get(self.llvm.ir.tys.find_or_insert(TyInfo::I8))
+                    .into_int_type()
+                    .const_int(*value as u64, false)
+                    .into(),
+                Value::Ref(pointer) => todo!(),
+                Value::Array(values) => todo!(),
+            },
+        }
+    }
+}
+
+struct LlvmTys<'ir, 'ctx> {
+    cache: RefCell<HashMap<Ty, BasicTypeEnum<'ctx>>>,
+    llvm: &'ctx Llvm<'ir>,
+}
+impl<'ir, 'ctx> LlvmTys<'ir, 'ctx> {
+    pub fn new(llvm: &'ctx Llvm<'ir>) -> Self {
+        Self {
+            cache: RefCell::new(HashMap::new()),
+            llvm,
+        }
+    }
+
+    pub fn get(&self, ty: Ty) -> BasicTypeEnum<'ctx> {
+        let cache = self.cache.borrow().get(&ty).cloned();
+        match cache {
+            Some(ty) => ty,
+            None => {
+                let mut cache = self.cache.borrow_mut();
+                cache.insert(ty, self.of_ty(ty));
+                cache[&ty]
+            }
+        }
+    }
+
+    fn of_ty(&self, ty: Ty) -> BasicTypeEnum<'ctx> {
+        match self.llvm.ir.tys.get(ty) {
+            TyInfo::U8 => self.llvm.ctx.i8_type().as_basic_type_enum(),
+            TyInfo::I8 => self.llvm.ctx.i8_type().as_basic_type_enum(),
+            TyInfo::Ref(ty) => todo!(),
+            TyInfo::Array { ty, length } => todo!(),
+        }
     }
 }
