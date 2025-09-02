130ba6f0dcdca3518fd2f1f16039636db93f411d
Tom Anderson <tom@ando.sh>
2025-09-02 21:28:04 +1000

    extra test cases

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![recursion_limit = "256"]
+
 mod interpreter;
 mod ir;
 mod llvm;
@@ -192,6 +194,232 @@ mod test {
 
             _2 = const 2u8;
             _0 = Add(_0, _1[_2]);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        deref_chained_ref => [10u8]
+
+        let _0: u8;
+        let _1: u8;
+        let _2: &u8;
+        let _3: & &u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = const 10u8;
+            StorageLive(_2);
+            _2 = &_1;
+            StorageLive(_3);
+            _3 = &_2;
+            _0 = **_3;
+            StorageDead(_3);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        deref_array_element_ref => [2u8]
+
+        let _0: u8;
+        let _1: [u8; 3];
+        let _2: u8;
+        let _3: &u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = [const 1u8, const 2u8, const 3u8];
+            StorageLive(_2);
+            _2 = const 1u8;
+            StorageLive(_3);
+            _3 = &_1[_2];
+            _0 = *_3;
+            StorageDead(_3);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        mutate_via_ref_and_return_orig => [99u8]
+
+        let _0: u8;
+        let _1: u8;
+        let _2: &u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = const 5u8;
+            StorageLive(_2);
+            _2 = &_1;
+            _1 = const 99u8;
+            _0 = *_2;
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        index_add_deref_to_element => [25u8]
+
+        let _0: u8;
+        let _1: [u8; 3];
+        let _2: u8;
+        let _3: &u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = [const 10u8, const 20u8, const 30u8];
+            StorageLive(_2);
+            _2 = const 1u8;
+            StorageLive(_3);
+            _3 = &_1[_2];
+            _0 = Add(*_3, const 5u8);
+            StorageDead(_3);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        add_deref_to_constant => [110u8]
+
+        let _0: u8;
+        let _1: u8;
+        let _2: &u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = const 100u8;
+            StorageLive(_2);
+            _2 = &_1;
+            _0 = Add(*_2, const 10u8);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        add_two_dereferenced_values => [12u8]
+
+        let _0: u8;
+        let _1: u8;
+        let _2: u8;
+        let _3: &u8;
+        let _4: &u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = const 5u8;
+            StorageLive(_2);
+            _2 = const 7u8;
+            StorageLive(_3);
+            _3 = &_1;
+            StorageLive(_4);
+            _4 = &_2;
+            _0 = Add(*_3, *_4);
+            StorageDead(_4);
+            StorageDead(_3);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        deref_array_then_add => [20u8]
+
+        let _0: u8;
+        let _1: [u8; 2];
+        let _2: u8;
+        let _3: &u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = [const 10u8, const 50u8];
+            StorageLive(_2);
+            _2 = const 0u8;
+            StorageLive(_3);
+            _3 = &_1[_2];
+            _0 = Add(*_3, const 10u8);
+            StorageDead(_3);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        deref_array_elements_add => [8u8]
+
+        let _0: u8;
+        let _1: [u8; 3];
+        let _2: u8;
+        let _3: u8;
+        let _4: &u8;
+        let _5: &u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = [const 1u8, const 3u8, const 5u8];
+            StorageLive(_2);
+            _2 = const 1u8;
+            StorageLive(_3);
+            _3 = const 2u8;
+            StorageLive(_4);
+            _4 = &_1[_2];
+            StorageLive(_5);
+            _5 = &_1[_3];
+            _0 = Add(*_4, *_5);
+            StorageDead(_5);
+            StorageDead(_4);
+            StorageDead(_3);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        add_deref_to_deref_chain => [130u8]
+
+        let _0: u8;
+        let _1: u8;
+        let _2: &u8;
+        let _3: & &u8;
+        let _4: u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = const 100u8;
+            StorageLive(_2);
+            _2 = &_1;
+            StorageLive(_3);
+            _3 = &_2;
+            StorageLive(_4);
+            _4 = **_3;
+            _0 = Add(const 30u8, _4);
+            StorageDead(_4);
+            StorageDead(_3);
+            StorageDead(_2);
+            StorageDead(_1);
+            return;
+        }
+    }
+
+    run_test! {
+        add_mixed_constants_and_locals => [24u8]
+
+        let _0: u8;
+        let _1: u8;
+        let _2: u8;
+        bb0: {
+            StorageLive(_1);
+            _1 = const 10u8;
+            StorageLive(_2);
+            _2 = const 11u8;
+            _0 = Add(const 3u8, _1);
+            _0 = Add(_0, _2);
             StorageDead(_2);
             StorageDead(_1);
             return;
