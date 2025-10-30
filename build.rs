abe6af31ba8f8deb701440bd96982a98699b8798
Tom Anderson <tom.anderson@canonical.com>
2025-10-30 13:40:33 +1100

    conditionally set LLVM sys prefix

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let prefix = if cfg!(target_os = "macos") {
+        "/opt/homebrew/opt/llvm@18"
+    } else {
+        "/usr/lib/llvm-18"
+    };
+
+    println!("cargo::rustc-env=LLVM_SYS_181_PREFIX={prefix}");
+}
