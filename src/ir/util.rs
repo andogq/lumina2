b5408aa35d2916bd765d69770053262f173108e4
Tom Anderson <tom@ando.sh>
2026-01-02 20:25:31 +1100

    fix `enum_conversion` macro formatting

--- src/ir/util.rs
+++ src/ir/util.rs
@@ -1,24 +1,25 @@
+/// Helper to implement `From` and `TryFrom` to/from an enum.
 #[macro_export]
 macro_rules! enum_conversion {
-        ([$target:ident] $($variant:ident: $ty:ty),* $(,)?) => {
-            $(
-                impl From<$ty> for $target {
-                    fn from(value: $ty) -> Self {
-                        Self::$variant(value)
+    ([$target:ident] $($variant:ident: $ty:ty),* $(,)?) => {
+        $(
+            impl From<$ty> for $target {
+                fn from(value: $ty) -> Self {
+                    Self::$variant(value)
+                }
+            }
+
+            impl TryFrom<$target> for $ty {
+                type Error = $target;
+
+                fn try_from(value: $target) -> Result<Self, $target> {
+                    #[allow(unreachable_patterns)]
+                    match value {
+                        $target::$variant(value) => Ok(value),
+                        expression => Err(expression)
                     }
                 }
-
-                impl TryFrom<$target> for $ty {
-                    type Error = $target;
-
-                    fn try_from(value: $target) -> Result<Self, $target> {
-                        #[allow(unreachable_patterns)]
-                        match value {
-                            $target::$variant(value) => Ok(value),
-                            expression => Err(expression)
-                        }
-                    }
-                }
-            )*
-        };
-    }
+            }
+        )*
+    };
+}
